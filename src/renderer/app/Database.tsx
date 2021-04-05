c85892ae15c0a2e22826a79160a33d9bd3b6ea82
ambareng <arvin.maltezo.bareng@gmail.com>
2021-04-05 21:54:42 +0800

    Added Ability to Show and Add Data Name, Surname

--- src/renderer/app/Database.tsx
+++ src/renderer/app/Database.tsx
@@ -1,5 +1,5 @@
 import { hot } from 'react-hot-loader/root';
-import React from 'react';
+import React, { useState } from 'react';
 import { remote } from 'electron';
 import Database from '../../main/database/Database';
 
@@ -8,19 +8,45 @@ export const Application: React.FC<{}> = () => {
 
     console.log(database);
 
-    async function testDatabase() {
-        const insert = await database.insert('test', 'test');
+    const [data, setData] = useState([]);
 
-        console.log('Insert: ');
-        console.table(insert);
-        console.log('Fetch: ');
-        console.table(await database.fetchAll());
+    async function insertToDatabase() {
+        const insert = await database.insert('First Name', 'Last Name');
+
+        // console.log('Insert: ');
+        // console.table(insert);
+        // console.log('Fetch: ');
+        // console.table(await database.fetchAll());
+
+        setData(await database.fetchAll());
     }
+
+    async function showDatabase() {
+        // const insert = await database.insert('First Name', 'Last Name');
+
+        // console.log('Insert: ');
+        // console.table(insert);
+        // console.log('Fetch: ');
+        // console.table(await database.fetchAll());
+
+        if (data.length <= 0) {
+            setData(await database.fetchAll());
+        }
+    }
+
+    showDatabase();
 
     return (
         <div>
-            <button onClick={() => testDatabase()}>test</button>
-            Database
+            <button onClick={() => insertToDatabase()}>test</button>
+            {data.map((d) => {
+                return (
+                    <div key={d.key}>
+                        <h1>Name: {d.name}</h1>
+                        <h2>Surname: {d.surname}</h2>
+                    </div>
+                );
+            })}
         </div>
     );
 };
